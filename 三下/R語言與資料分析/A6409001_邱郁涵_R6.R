e72813a5824fc5413e1a4ed1c5ce07b1c484a1b6
A5254135 <A5254135>
2019-04-25 19:40:56 +0800

    2019/04/25_19:40

--- 三下/R語言與資料分析/A6409001_邱郁涵_R6.R
+++ 三下/R語言與資料分析/A6409001_邱郁涵_R6.R
@@ -1,12 +1,36 @@
 
 recursive.factorial   <- function(x)
 {
-  testSeq             <- c(0)
-
-
-  if     (x <= 2) return (append(testSeq, recursive.factorial(x-1)))
-  else            return (append(testSeq, (recursive.factorial(x-1)+testSeq[x-1])))
-
+  if     (x <= 2)
+  {
+    return (1)
+  }
+  else
+  {
+    return (recursive.factorial(x-2)+recursive.factorial(x-1))
+  }
 }
 
-print(recursive.factorial(3))
+print(recursive.factorial(5))
+
+Fibonacci <- function(n)
+{
+  f1 <- 1
+  f2 <- 1
+  if(n == 1|n == 2)
+  {
+    return(f2)
+  }
+  else
+  {
+    for(i in 3:n)
+    {
+      temp1 <- f2
+      f2 <- f1 + f2
+      f1 <- temp1
+    }
+    return(f2)
+  }
+}
+
+print(Fibonacci(6))

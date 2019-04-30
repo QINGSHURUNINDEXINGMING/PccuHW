5cbede8a6f413279358c6060eedeee1e99feb551
Your Name <you@example.com>
2019-04-30 16:23:22 +0800

    2019/04/30_16:25

--- 三下/智慧型手機程式設計/HW2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ 三下/智慧型手機程式設計/HW2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.myapplication;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button chooseButton;
+    private int year, month, day;
+
+    private void processViews()
+    {
+        chooseButton=(Button)findViewById(R.id.chooseButton);
+    }
+
+    public void clickButton01(View view)          //選擇日期
+    {
+
+
+
+
+
+
+    }
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+
+
     }
 }

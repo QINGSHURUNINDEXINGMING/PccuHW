aed1fa62a685fe9f4cdd5770c94f08d76dc960db
QINGSHURUNINDEXINGMING <A6409001@g.pccu.edu.tw>
2019-06-12 15:05:29 +0800

    2019/06/12_15:05

--- 三下/基礎雲端運算程式設計/HW03/HW03_API/Controllers/BMIController.cs
+++ 三下/基礎雲端運算程式設計/HW03/HW03_API/Controllers/BMIController.cs
@@ -14,8 +14,15 @@ namespace HW03_API.Controllers
         public IHttpActionResult result(double cm, double kg)
         {
             // 建立記帳簿服務結果資料物件
-            BMIResultData BMIResultDataOBJ = new BMIResultData();
-            return Ok(kg / Math.Pow(Math.Round(cm / 100, 2), 2));
+            BMIResultData BMIResultDataobj = new BMIResultData();
+
+            //=== 並將BMI服務之運算結果存入BMI服務結果資料物件對應的欄位 ====
+            BMIResultDataobj.Status = "OK";
+            BMIResultDataobj.BMIResult = kg / Math.Pow(Math.Round(cm / 100, 2), 2);
+
+            // 回傳BMI服務結果資料物件，執行環境會因應Client端的請求(利用application/json或application/xml)
+            // 分別自動地將物件序列化(Serialize)成JSON或XML字串
+            return Ok(BMIResultDataobj);
         }
 
 
@@ -24,7 +31,16 @@ namespace HW03_API.Controllers
         [HttpPost]
         public IHttpActionResult resultPost(double cm, double kg)
         {
-            return Ok(kg / Math.Pow(Math.Round(cm / 100, 2), 2));
+            // 建立記帳簿服務結果資料物件
+            BMIResultData BMIResultDataobj = new BMIResultData();
+
+            //=== 並將BMI服務之運算結果存入BMI服務結果資料物件對應的欄位 ====
+            BMIResultDataobj.Status = "OK";
+            BMIResultDataobj.BMIResult = kg / Math.Pow(Math.Round(cm / 100, 2), 2);
+
+            // 回傳BMI服務結果資料物件，執行環境會因應Client端的請求(利用application/json或application/xml)
+            // 分別自動地將物件序列化(Serialize)成JSON或XML字串
+            return Ok(BMIResultDataobj);
         }
 
         // 用來儲存進位服務結果資料的內部類別，
@@ -34,8 +50,7 @@ namespace HW03_API.Controllers
             // 儲存BMI服務結果之狀態
             public string Status { get; set; }
             // 儲存BMI服務之運算結果
-            public string BMIResult { get; set; }
+            public double BMIResult { get; set; }
         }
-
     }
 }
--- 三下/基礎雲端運算程式設計/HW03/HW03_API/Controllers/CarryController.cs
+++ 三下/基礎雲端運算程式設計/HW03/HW03_API/Controllers/CarryController.cs
@@ -9,9 +9,6 @@ namespace HW03_API.Controllers
 {
     public class CarryController : ApiController
     {
-        // 建立進位服務結果資料物件
-        CarryResultData carryResultDataObj = new CarryResultData();
-
         // 產生二進位方法
         [Route("api/Carry/Binary/{num}")]
         [HttpGet]
@@ -118,16 +115,6 @@ namespace HW03_API.Controllers
             // 回傳進位服務結果資料物件，執行環境會因應Client端的請求(利用application/json或application/xml)
             // 分別自動地將物件序列化(Serialize)成JSON或XML字串
             return Ok(carryResultDataObj);
-        }
-
-        // 用來儲存進位服務結果資料的內部類別，
-        // 每一個欄位變數名稱將是回傳JSON物件的Key
-        public class CarryResultData
-        {
-            // 儲存進位服務結果之狀態
-            public string Status { get; set; }
-            // 儲存進位服務之運算結果
-            public string CarryResult { get; set; }
         }
     }
 }

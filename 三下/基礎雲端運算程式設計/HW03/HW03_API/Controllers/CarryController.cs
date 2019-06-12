6a57d78daa9eed7e5327bbbb996ecef15881a52b
QINGSHURUNINDEXINGMING <A6409001@g.pccu.edu.tw>
2019-06-12 16:41:20 +0800

    2019/06/12_16:41

--- 三下/基礎雲端運算程式設計/HW03/HW03_API/Controllers/CarryController.cs
+++ 三下/基礎雲端運算程式設計/HW03/HW03_API/Controllers/CarryController.cs
@@ -16,15 +16,7 @@ namespace HW03_API.Controllers
         {
             string result;
             result = Convert.ToString(num, 2);
-            //return Ok(result);
-
-            // 並將進位服務之運算結果存入進位服務結果資料物件對應的欄位 ====
-            carryResultDataObj.Status = "OK";
-            carryResultDataObj.CarryResult = result;
-
-            // 回傳進位服務結果資料物件，執行環境會因應Client端的請求(利用application/json或application/xml)
-            // 分別自動地將物件序列化(Serialize)成JSON或XML字串
-            return Ok(carryResultDataObj);
+            return Ok(result);
         }
 
         // 產生八進位方法
@@ -34,15 +26,7 @@ namespace HW03_API.Controllers
         {
             string result;
             result = Convert.ToString(num, 8);
-            //return Ok(result);
-
-            // 並將進位服務之運算結果存入進位服務結果資料物件對應的欄位 ====
-            carryResultDataObj.Status = "OK";
-            carryResultDataObj.CarryResult = result;
-
-            // 回傳進位服務結果資料物件，執行環境會因應Client端的請求(利用application/json或application/xml)
-            // 分別自動地將物件序列化(Serialize)成JSON或XML字串
-            return Ok(carryResultDataObj);
+            return Ok(result);
         }
 
         // 產生十六進位方法
@@ -52,69 +36,7 @@ namespace HW03_API.Controllers
         {
             string result;
             result = Convert.ToString(num, 16);
-            //return Ok(result);
-
-            // 並將進位服務之運算結果存入進位服務結果資料物件對應的欄位 ====
-            carryResultDataObj.Status = "OK";
-            carryResultDataObj.CarryResult = result;
-
-            // 回傳進位服務結果資料物件，執行環境會因應Client端的請求(利用application/json或application/xml)
-            // 分別自動地將物件序列化(Serialize)成JSON或XML字串
-            return Ok(carryResultDataObj);
-        }
-
-        // 產生二進位方法
-        [Route("api/Carry/BinaryPost/{num}")]
-        [HttpPost]
-        public IHttpActionResult BinaryPost(int num)
-        {
-            string result;
-            result = Convert.ToString(num, 2);
-            //return Ok(result);
-
-            // 並將進位服務之運算結果存入進位服務結果資料物件對應的欄位 ====
-            carryResultDataObj.Status = "OK";
-            carryResultDataObj.CarryResult = result;
-
-            // 回傳進位服務結果資料物件，執行環境會因應Client端的請求(利用application/json或application/xml)
-            // 分別自動地將物件序列化(Serialize)成JSON或XML字串
-            return Ok(carryResultDataObj);
-        }
-
-        // 產生八進位方法
-        [Route("api/Carry/OctalPost/{num}")]
-        [HttpPost]
-        public IHttpActionResult OctalPost(int num)
-        {
-            string result;
-            result = Convert.ToString(num, 8);
-            //return Ok(result);
-
-            // 並將進位服務之運算結果存入進位服務結果資料物件對應的欄位 ====
-            carryResultDataObj.Status = "OK";
-            carryResultDataObj.CarryResult = result;
-
-            // 回傳進位服務結果資料物件，執行環境會因應Client端的請求(利用application/json或application/xml)
-            // 分別自動地將物件序列化(Serialize)成JSON或XML字串
-            return Ok(carryResultDataObj);
-        }
-
-        // 產生十六進位方法
-        [Route("api/Carry/HexadecimalPost/{num}")]
-        [HttpPost]
-        public IHttpActionResult HexadecimalPost(int num)
-        {
-            string result;
-            result = Convert.ToString(num, 16);
-            //return Ok(result);
-
-            // 並將進位服務之運算結果存入進位服務結果資料物件對應的欄位 ====
-            carryResultDataObj.Status = "OK";
-            carryResultDataObj.CarryResult = result;
-
-            // 回傳進位服務結果資料物件，執行環境會因應Client端的請求(利用application/json或application/xml)
-            // 分別自動地將物件序列化(Serialize)成JSON或XML字串
-            return Ok(carryResultDataObj);
+            return Ok(result);
         }
     }
 }

514a0c6460f25e3025c9733f2c4921026e02a578
QINGSHURUNINDEXINGMING <A6409001@g.pccu.edu.tw>
2019-06-21 15:32:09 +0800

    2019/06/21_15:32

---
+++ 三下/基礎雲端運算程式設計/HW03/HW03_MVC/ViewModels/BMIViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HW03_MVC.ViewModels
+{
+    public class BMIViewModel
+    {
+        [DisplayName("身高：")]
+        [Required(ErrorMessage = "請輸入身高")]
+        public double CM { get; set; }
+
+        [DisplayName("體重：")]
+        [Required(ErrorMessage = "請輸入體重")]
+        public double KG { get; set; }
+
+        [DisplayName("結果：")]
+        public string Result { get; set; }
+    }
+}

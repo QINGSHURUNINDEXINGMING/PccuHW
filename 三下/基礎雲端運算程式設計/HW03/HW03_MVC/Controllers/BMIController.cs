22d7b9c85470849ff8948178f86e006ab998bba3
QINGSHURUNINDEXINGMING <A6409001@g.pccu.edu.tw>
2019-06-22 16:44:12 +0800

    2019/06/22_16:44

--- 三下/基礎雲端運算程式設計/HW03/HW03_MVC/Controllers/BMIController.cs
+++ 三下/基礎雲端運算程式設計/HW03/HW03_MVC/Controllers/BMIController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -55,25 +56,26 @@ namespace HW03_MVC.Controllers
         }
 
         // POST: BMI
-        public ActionResult BMIPost()
+        public ActionResult BMIComputePost()
         {
             return View();
         }
 
         [HttpPost]
-        public async Task<ActionResult> BMIPost([Bind(Exclude = "Result")] BMIViewModel bmidata)
+        public async Task<ActionResult> BMIComputePost([Bind(Exclude = "result")] BMIViewModel bmidata)
         {
             string result = "", urlstring = "";
+
             if (!ModelState.IsValid)
             {
                 return View(bmidata);
             }
             try
             {
-                urlstring = "http://140.137.41.136:5558/a1234567/MathLotteryWebAPIs/api/lottery/" + type + "/" + bmidata.Sets;
+                urlstring = "http://140.137.41.136:5558/A6409001/HW03/api/BMI/" + bmidata.cm + " / " + bmidata.kg;
                 //建立HttClient物件
                 client = new HttpClient();
-                // 將lotterydata物件序列化成JSON格式字串
+                // 將bmidata物件序列化成JSON格式字串
                 dynamic jsonString = JsonConvert.SerializeObject(bmidata);
                 // 建立傳遞內容HttpContent物件
                 HttpContent contentPost = new StringContent(jsonString, Encoding.UTF8, "application/json");
@@ -83,16 +85,16 @@ namespace HW03_MVC.Controllers
                 result = await response.Content.ReadAsStringAsync();
                 // 將JSON格式字串轉換成.NET的JSON物件
                 dynamic jsonObject = JsonConvert.DeserializeObject(result);
-                // 從JSON物件中名稱為LotteryResult的值，存入lotterydata物件的Result欄位
-                bmidata.result = jsonObject.LotteryResult;
-                // 將lotterydata物件傳送到View顯示
+                // 從JSON物件中名稱為BMIResult的值，存入bmidata物件的result欄位
+                bmidata.result = jsonObject.BMIResult;
+                // 將bmidata物件傳送到View顯示
                 return View(bmidata);
             }
             catch (Exception ex)
             {
-                result = "產生樂透號碼時發生例外，原因如下: " + ex.Message;
+                result = "發生例外，原因如下: " + ex.Message;
                 bmidata.result = result;
-                // 將lotterydata物件傳送到View顯示
+                // 將bmidata物件傳送到View顯示
                 return View(bmidata);
             }
         }

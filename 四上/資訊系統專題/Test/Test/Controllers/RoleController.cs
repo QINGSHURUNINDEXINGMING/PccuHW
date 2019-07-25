c0570501faf0f7921ffb18caf21251e0f96bd9bf
QINGSHURUNINDEXINGMING <A6409001@g.pccu.edu.tw>
2019-07-25 18:13:11 +0800

    2019/07/25_18:13

--- 四上/資訊系統專題/Test/Test/Controllers/RoleController.cs
+++ 四上/資訊系統專題/Test/Test/Controllers/RoleController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity.Owin;
@@ -46,6 +47,42 @@ namespace Test.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(RoleViewModel model)
+        {
+            var role = new ApplicationRole() { Name = model.Name };
+            await RoleManager.CreateAsync(role);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<ActionResult>Edit(string id)
+        {
+            var role = await RoleManager.FindByIdAsync(id);
+            return View(new RoleViewModel(role));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(RoleViewModel model)
+        {
+            var role = new ApplicationRole() { Id = model.Id, Name = model.Name };
+            await RoleManager.UpdateAsync(role);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<ActionResult> Details(string id)
+        {
+            var role = await RoleManager.FindByIdAsync(id);
+            return View(new RoleViewModel(role));
+        }
+
+
+
+
+
+
+
+
 
 
 

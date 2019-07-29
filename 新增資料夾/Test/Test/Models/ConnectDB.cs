473d17d08a57fd1ae329df5b5140a78c36589e4f
QINGSHURUNINDEXINGMING <A6409001@g.pccu.edu.tw>
2019-07-29 16:01:43 +0800

    2019/07/29_16:01

---
+++ 新增資料夾/Test/Test/Models/ConnectDB.cs
@@ -0,0 +1,22 @@
+namespace Test.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class ConnectDB : DbContext
+    {
+        public ConnectDB()
+            : base("name=ConnectDB")
+        {
+        }
+
+        public DbSet<QR_CODE> QR_code1 { get; set; }
+        public DbSet<GOOD> Good1 { get; set; }
+        public DbSet<HISTORY> History1 { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}

cd54d734634c61471eb2f2b81904134fd3a8c1fd
QINGSHURUNINDEXINGMING <A6409001@g.pccu.edu.tw>
2019-08-07 15:51:08 +0800

    2019/08/07_15:51

--- TestAll/TestAll/Controllers/AdminController.cs
+++ TestAll/TestAll/Controllers/AdminController.cs
@@ -1,14 +1,18 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TestAll.Models;
 
 namespace TestAll.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
-        [Authorize(Roles ="Admin")]
+        ApplicationDbContext context = new ApplicationDbContext();
         // GET: Admin
         public ActionResult Index()
         {
@@ -16,6 +20,23 @@ namespace TestAll.Controllers
         }
         public ActionResult CreateUser()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateUser(FormCollection form)
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            string userName = form["txtEmail"];
+            string email = form["txtEmail"];
+            string pwd = form["txtPassword"];
+
+            var user = new ApplicationUser();
+            user.UserName = userName;
+            user.Email = email;
+
+            var newUser = userManager.Create(user, pwd);
+
             return View();
         }
         public ActionResult AssignRole()

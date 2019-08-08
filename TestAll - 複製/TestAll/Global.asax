7446843a012381bbde3c28474bc4339408fa21b6
QINGSHURUNINDEXINGMING <A6409001@g.pccu.edu.tw>
2019-08-08 15:48:57 +0800

    2019/08/08_15:48

---
+++ TestAll - 複製/TestAll/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestAll.MvcApplication" Language="C#" %>
---
+++ TestAll - 複製/TestAll/Migrations/201908080720553_GOOD.cs
@@ -0,0 +1,30 @@
+namespace TestAll.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.GOODs",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        UID = c.String(nullable: false),
+                        name = c.String(nullable: false),
+                        price = c.Int(nullable: false),
+                        discount = c.Int(nullable: false),
+                        discount_price = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.GOODs");
+        }
+    }
+}

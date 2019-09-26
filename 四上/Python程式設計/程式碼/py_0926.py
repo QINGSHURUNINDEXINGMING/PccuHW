2b9d3b9f9f65b3b715c6065cfcfc6e48bd241822
QINGSHURUNINDEXINGMING <A6409001@g.pccu.edu.tw>
2019-09-26 18:06:20 +0800

    2019/09/26_18:06

---
+++ 四上/Python程式設計/程式碼/py_0926.py
@@ -0,0 +1,66 @@
+#0926
+
+print("""You can't improve your past,
+but you can improve your future.
+Once time is wasted, life is wasted.\n""")
+
+
+print('''I'm a "rich" man.\n''')
+
+
+print("""'''I'm a "rich" man.'''\n""")
+
+
+#print(y=a=5+6)
+#print(a=5+6)
+
+print(print(6))
+
+y=4.3
+print(y)
+
+int(y)
+print(y)
+
+
+print(type(round(2.01*100)))
+print(type(round(2.013*100, 2)))
+
+
+#inputNum=int(input("Enter the nuumber:　"))
+
+#hour=inputNum//60
+#minute=inputNum%60
+
+#print(hour, 'h', minute, 'm')
+#print(str(hour)+'h'+str(minute)+'m')
+
+inputNum=int(input("Enter the nuumber:　"))
+
+oneThousand=inputNum//1000
+oneHundred=inputNum%1000//100
+tenDollar=inputNum%100//10
+
+print('oneThousand=', oneThousand, 'oneHundred=', oneHundred, 'tenDollar=', tenDollar)
+
+s='he is a good man.'
+
+print(s[1:9:3])
+print(s[1:17:3])
+
+print(s[-1:-18:-3])
+
+print(len(s))
+
+
+
+
+
+
+
+
+
+
+
+
+

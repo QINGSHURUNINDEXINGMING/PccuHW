d4b8e9ba9aa079d9e32c483b8ba39172bffc143c
QINGSHURUNINDEXINGMING <A6409001@g.pccu.edu.tw>
2019-10-14 18:20:53 +0800

    2019/10/14_18:20

---
+++ 四上/網頁程式設計(二)/A6409001_WebPD_Week6/A6409001_WebPD_Week6/Controllers/A6409001_WebPD_Week6Controller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace A6409001_WebPD_Week6.Controllers
+{
+    public class A6409001_WebPD_Week6Controller : Controller
+    {
+        // GET: A6409001_WebPD_Week6
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Grid1()
+        {
+            return View();
+        }
+        public ActionResult Grid2()
+        {
+            return View();
+        }
+        public ActionResult Razor1()
+        {
+            return View();
+        }
+        public ActionResult Razor2()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
---
+++ 四上/網頁程式設計(二)/A6409001_WebPD_Week6/A6409001_WebPD_Week6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="A6409001_WebPD_Week6.MvcApplication" Language="C#" %>

d71e31692859567d1a62433b98937683178f3286
QINGSHURUNINDEXINGMING <A6409001@g.pccu.edu.tw>
2019-10-17 18:20:39 +0800

    2019/10/17_18:20

---
+++ 四上/Python程式設計/程式碼/py_1017.py
@@ -0,0 +1,14 @@
+num1=int(input("Number1? "))
+if num1 > 50:
+      num2=int(input("NUmber2? "))
+      print("num1 / num2 = ", num1//num2 )
+elif num1 < 10:
+      string1=str(input("String? "))
+      print("num1 * string1 = " + num1*string1)
+else:
+      num2=int(input("NUmber2? "))
+      if num2 > 5:
+            print("num1 + num2 = ", (num1+num2))
+      else:
+            print("num1 * num2 = ", (num1*num2))
+

bd3a10b7c0b398e473ba9b097d8667a93140229a
QINGSHURUNINDEXINGMING <A6409001@g.pccu.edu.tw>
2019-10-19 22:52:19 +0800

    2019/10/19_22:52

---
+++ 四上/資訊系統專題/自己/testNFC/testNFC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(testNFC.Startup))]
+namespace testNFC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
